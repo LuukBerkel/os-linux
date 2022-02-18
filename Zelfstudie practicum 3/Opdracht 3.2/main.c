1e194717e26bbd1de08851d4fb1b685df699f817
luuki <lah.vanberkel@student.avans.nl>
2022-02-18 19:42:24 +0100

    Fixing multiple reading mistake

--- Zelfstudie practicum 3/Opdracht 3.2/main.c
+++ Zelfstudie practicum 3/Opdracht 3.2/main.c
@@ -4,9 +4,11 @@
 #include "esp_system.h"
 #include "freertos/semphr.h"
 
+
+
 char* string = "test_string";
 
-SemaphoreHandle_t handle;
+SemaphoreHandle_t string_acces_mutex;
 
 TaskHandle_t reader1;
 TaskHandle_t reader2;
@@ -17,11 +19,14 @@ TaskHandle_t writer3;
 void readerTask(void *parameter) {
     portTickType xLastWakeTime = xTaskGetTickCount();
 
+
     while (true) {
         vTaskDelayUntil(&xLastWakeTime, 1000 / portTICK_RATE_MS);
-        xSemaphoreTake(handle, portMAX_DELAY);
-        printf("Reading %s: %s\n", (char *) parameter, string);
-        xSemaphoreGive(handle);
+
+        //Checking wether the mutex is taken
+        if(xQueuePeek((xQueueHandle)string_acces_mutex,(void *)NULL,(portTickType)NULL) == pdTRUE){
+            printf("Reading %s: %s\n", (char *) parameter, string);
+        }
     }
 }
 
@@ -30,15 +35,15 @@ void writingTask(void *parameter) {
 
     while (true) {
         vTaskDelayUntil(&xLastWakeTime, 1000 / portTICK_RATE_MS);
-        xSemaphoreTake(handle, portMAX_DELAY);
+        xSemaphoreTake(string_acces_mutex, portMAX_DELAY);
         string = (char *) parameter;
         printf("Writing: %s\n", string);
-        xSemaphoreGive(handle);
+        xSemaphoreGive(string_acces_mutex);
     }
 }
 
 void app_main() {
-    handle = xSemaphoreCreateMutex();
+    string_acces_mutex = xSemaphoreCreateMutex();
 
     xTaskCreatePinnedToCore(readerTask, "Reader 1", 10000, "Reader 1", 1, reader1, 0);
     xTaskCreatePinnedToCore(readerTask, "Reader 2", 10000, "Reader 2", 1, reader2, 0);
